7ecbff13b7d25249b7268f2d147e5e434f3efdd9
Josep <josep.alos.pascual@hotmail.com>
2019-10-12 01:17:01 +0200

    Created models for session and preference

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Session(models.Model):
+    session_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class Preference(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name="preferences")
+    preference_name = models.CharField(max_length=30, blank=False, null=False)
+    value = models.TextField(max_length=100, blank=True, null=True)
