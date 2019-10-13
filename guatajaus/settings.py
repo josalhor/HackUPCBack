c0003667139ecbfcdee57c5e245abce162bb2eca
Josep <josep.alos.pascual@hotmail.com>
2019-10-13 03:38:24 +0200

    Recover cors

--- guatajaus/settings.py
+++ guatajaus/settings.py
@@ -127,8 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Celery
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
-CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
-CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+#REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+#REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
+#CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+#CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+#CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
